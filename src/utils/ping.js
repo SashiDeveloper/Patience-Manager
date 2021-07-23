2879622231fbe07ff1cc722397f9a7bf6a82cb2d
Minna <65752591+MKIsHereOficial@users.noreply.github.com>
2021-07-22 22:09:45 -0300

    Create ping.js

---
+++ src/utils/ping.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
